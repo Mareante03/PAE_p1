e440c6def39212879f57339bc9264c0a7ce5fc05
AnxoTrillo <anxo.trillo@rai.usc.es>
2025-02-03 11:27:25 +0100

    hoe

--- daxpy.c
+++ daxpy.c
@@ -1,31 +1,51 @@
 #include "stdio.h"
 #include "stdlib.h"
+#define GIB_SIZE (1024L * 1024L * 1024L)
+
+void daxpy(double *arrX, double *arrY, double alpha, size_t N){
+    for(size_t i = 0; i < N; i++){
+        arrY[i] = alpha * arrX[i] + arrY[i];
+    }
+}
 
 
-void main(int argc, char** argv){
-k
+int main(int argc, char** argv){
+    unsigned long N = 0;
 
-    if(argc!=3){
-        exit(0);}
+    switch(argc) {
+        case 2:
+            N = 6;
+            break;
+        case 3:
+            N = atol(argv[2]);
+            break;
+        default:
+            printf("ERROR: ARGUMENTOS INSUFICIENTES");
+            exit(69);
+            break;
+    }
+    double alpha = atof(argv[1]);
+    size_t num_elements = N * GIB_SIZE / sizeof(double);
 
 
-    switch(argc){
+    double *arrX = malloc(num_elements * sizeof(double));
+    double *arrY = malloc(num_elements * sizeof(double));
 
-    int alpha = atoi(argv[1]);
-    int N = atoi(argv[2]);
-
-
-    size_t N = (1073741824 * 6 / sizeof(float));
-
-    
-
-    int *arrX=malloc((int) N*sizeof(int));
-    int *arrY= malloc((int) N*sizeof(int));
-
-    for(int i=0;i<N;i++){
-        arrX[i]=rand();
-        arrY[i]=rand();
+    if(arrX <= 0 || arrY <= 0){
+        printf("*EXTREMELY LOUD INCORRECT BUZZER*\npum explotó\n");
     }
 
+    printf("Alfa: %f\n", alpha);
 
+    for(int i = 0; i < num_elements; i++){
+        arrX[i] = rand();
+        arrY[i] = rand();
+    }
+
+    daxpy(arrX, arrY, alpha, num_elements);
+
+    printf("random = %f\n", arrY[rand()]);
+
+    free(arrX);
+    free(arrY);
 }
